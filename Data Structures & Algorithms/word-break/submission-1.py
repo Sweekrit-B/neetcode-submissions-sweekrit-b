7810511b4f50080d2b72527ef894ff1801ad87ad
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-06-22 23:02:31 -0600

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        starting_points = [0]
+
+        for ix in range(len(s)):
+            for sp in starting_points:
+                # print(f"Currently looking at {s[sp:ix + 1]}")
+                if s[sp:ix + 1] in wordDict:
+                    # print(f"{s[sp:ix + 1]} in wordDict")
+                    starting_points.append(ix + 1)
+                    break
+        
+        # print(starting_points)
+        if starting_points[-1] == len(s):
+            return True
+        return False
