b1f7a1a7c344e6df0a246c696f33d1a479a6ee80
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-07-09 19:42:57 -0700

    Add: max-area-of-island - submission-3

---
+++ Data Structures & Algorithms/max-area-of-island/submission-3.py
@@ -0,0 +1,45 @@
+#DFS
+
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        all_island_cells = set()
+        for i in range(len(grid)): # for every row
+            for j in range(len(grid[0])): # for every col
+                if grid[i][j] == 1:
+                    all_island_cells.add((i, j))
+
+        # print("All island cells before processing: ", all_island_cells)
+        max_island_area = 0
+
+        visited = set()
+
+        def dfs(current_cell, visited):
+            visited.add(current_cell)
+
+            neighbors = [
+                (current_cell[0] + 1, current_cell[1]),
+                (current_cell[0] - 1, current_cell[1]),
+                (current_cell[0], current_cell[1] + 1),
+                (current_cell[0], current_cell[1] - 1)
+            ]
+
+            for neighbor in neighbors:
+                    if neighbor in all_island_cells and neighbor not in visited:
+                        all_island_cells.remove(neighbor)
+                        dfs(neighbor, visited)
+
+        while len(all_island_cells) > 0:
+            start_val = all_island_cells.pop()
+            # print("Starting cell of this run: ", curr_queue)
+            visited = set()
+            dfs(start_val, visited)
+            
+            # print("All island cells visited while processing: ", visited)
+            # print("All remaining island cells: ", all_island_cells)
+
+            max_island_area = max(max_island_area, len(visited))
+            # print("Max island area: ", max_island_area)
+
+        # print("Final island cells: ", all_island_cells)
+        return max_island_area
+        
