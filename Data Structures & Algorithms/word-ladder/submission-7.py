5fb35a1c5a037992565b8f1d89c0b1b6db1ff87b
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-07-27 11:43:32 -0700

    Add: word-ladder - submission-7

---
+++ Data Structures & Algorithms/word-ladder/submission-7.py
@@ -0,0 +1,26 @@
+import string
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        one_letter_away_map = defaultdict(list)
+
+        def one_letter_diff(w1, w2):
+            mismatches = sum(c1 != c2 for c1, c2 in zip(w1, w2))
+            return mismatches == 1
+        
+        for w1 in wordList + [beginWord, endWord]:
+            one_letter_away_map[w1] = [w2 for w2 in wordList if one_letter_diff(w1, w2)]
+        
+        queue = deque([(beginWord, 1)])
+        visited = {beginWord}
+
+        while len(queue) != 0:
+            word, length = queue.popleft()
+            if word == endWord:
+                return length
+            for next_word in one_letter_away_map[word]:
+                if next_word not in visited: # if we have not already seen this (if we have, we already got shortest)
+                    visited.add(next_word) 
+                    queue.append((next_word, length + 1))
+        
+        return 0
