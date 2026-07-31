aa7d769c911f04b15a8e6da7bc1d42c27c056ea1
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-07-31 14:18:09 -0700

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,34 @@
+# Caching DFS solution
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        cache = {}
+
+        def dfs(val, level):
+            if (val, level) in cache:
+                return cache[(val, level)]
+            # print(f"Level {level}, looking at {val}")
+            if val == 0 and level == len(nums):
+                # print(f"Found a valid sequence, bubbling back up")
+                return 1
+            if val != 0 and level == len(nums):
+                # print("Found an invalid sequence, bubbling back up")
+                return 0
+
+            num = nums[level]
+            # print(f"Number of interest: {num}")
+            branch_1_root = val - num
+            # print(f"Branch 1 root: {branch_1_root}")
+            branch_2_root = val + num
+            # print(f"Branch 2 root: {branch_2_root}")
+
+            cache[(branch_1_root, level + 1)] = dfs(branch_1_root, level + 1)
+            # print(f"Value of DFS of branch 1: {cache[(branch_1_root, level + 1)]}")
+            cache[(branch_2_root, level + 1)] = dfs(branch_2_root, level + 1)
+            # print(f"Value of DFS of branch 2: {cache[(branch_2_root, level + 1)]}")
+
+            cache[(val, level)] = cache[(branch_1_root, level + 1)] + cache[(branch_2_root, level + 1)]
+            # print(f"Final DFS: {cache[(val, level)]}")
+            return cache[(val, level)]
+        
+        return dfs(target, 0)
