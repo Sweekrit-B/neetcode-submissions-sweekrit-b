c9e61aa92c8e3750e75314eb0b3a631c1fef1cd0
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-07-31 14:30:54 -0700

    Add: target-sum - submission-6

---
+++ Data Structures & Algorithms/target-sum/submission-6.py
@@ -0,0 +1,15 @@
+# Bottom up memoization two rows
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        dp = defaultdict(int)
+        dp[0] = 1 # (0 sum) -> 1 way
+
+        for i in range(len(nums)):
+            next_dp = defaultdict(int)
+            for cur_sum, count in dp.items():
+                next_dp[cur_sum + nums[i]] += count
+                next_dp[cur_sum - nums[i]] += count
+            dp = next_dp
+
+        return dp[target]
