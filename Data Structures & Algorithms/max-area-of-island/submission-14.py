925c64de5616198e99905cd9f326dad64cbbb597
Sweekrit-B <63749658+Sweekrit-B@users.noreply.github.com>
2026-08-06 22:46:07 -0700

    Add: max-area-of-island - submission-14

---
+++ Data Structures & Algorithms/max-area-of-island/submission-14.py
@@ -0,0 +1,42 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        all_land_cells = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    all_land_cells.add((i, j))
+        # print(all_land_cells)
+        
+        visited = set()
+        island_size = 1
+
+        def dfs(cell):
+            nonlocal visited
+            nonlocal island_size
+            
+            row, col = cell
+            neighbors = [
+                (row, col + 1),
+                (row, col - 1),
+                (row + 1, col),
+                (row - 1, col)
+            ]
+            for neighbor in neighbors:
+                if neighbor in all_land_cells and neighbor not in visited:
+                    visited.add(neighbor)
+                    island_size += 1
+                    dfs(neighbor)
+        
+        max_island_size = 0
+        for land_cell in all_land_cells:
+            if land_cell not in visited:
+                # reset current island size
+                island_size = 1
+                # add to visited
+                visited.add(land_cell)
+                # run the dfs
+                dfs(land_cell)
+                # determine if the island has grown
+                max_island_size = max(island_size, max_island_size)
+        
+        return max_island_size
